a583d859cb559f023521cbfd72718a628cfa317a
akhier <akhierthedragonhearted@yahoo.com>
2014-05-10 01:51:00 -0400

    Base Setup

---
+++ drw_sdl2.cpp
@@ -0,0 +1,11 @@
+#include "drw_sdl2.h"
+
+DrW_SDL2::DrW_SDL2()
+{
+    //ctor
+}
+
+DrW_SDL2::~DrW_SDL2()
+{
+    //dtor
+}
---
+++ drw_sdl2.h
@@ -0,0 +1,14 @@
+#ifndef DRW_SDL2_H
+#define DRW_SDL2_H
+
+
+class DrW_SDL2
+{
+    public:
+        DrW_SDL2();
+        virtual ~DrW_SDL2();
+    protected:
+    private:
+};
+
+#endif // DRW_SDL2_H
