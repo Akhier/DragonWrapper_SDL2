29376bece61e703f086cb6de3165da8345b3c412
akhier <akhierthedragonhearted@yahoo.com>
2014-05-10 20:20:40 -0400

    filling the constructor

--- drw_sdl2.cpp
+++ drw_sdl2.cpp
@@ -2,7 +2,12 @@
 
 DrW_SDL2::DrW_SDL2()
 {
-    //ctor
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+       _logerror("SDL_Init");
+    }
+    if (TTF_Init() != 0){
+       _logerror("TTF_Init");
+    }
 }
 
 DrW_SDL2::~DrW_SDL2()
