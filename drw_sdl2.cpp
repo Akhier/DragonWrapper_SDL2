de308dfba552d0ab39e4b5d9051ba05083f637a3
akhier <akhierthedragonhearted@yahoo.com>
2014-05-10 20:32:54 -0400

    forgot IMG_Init

--- drw_sdl2.cpp
+++ drw_sdl2.cpp
@@ -5,6 +5,9 @@ DrW_SDL2::DrW_SDL2()
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
        _logerror("SDL_Init");
     }
+    if (IMG_Init(IMG_INIT_PNG) != 0){
+        _logerror("IMG_Init");
+    }
     if (TTF_Init() != 0){
        _logerror("TTF_Init");
     }
@@ -12,6 +15,10 @@ DrW_SDL2::DrW_SDL2()
 
 void DrW_SDL2::_logerror(const std::string &message){
     std::cout << message << " Error: " << SDL_GetError() << std::endl;
+}
+
+SDL_Texture* DrW_SDL2::_loadtexture(const std::string &file){
+    SDL_Texture* outputTexture = IMG
 }
 
 DrW_SDL2::~DrW_SDL2()
