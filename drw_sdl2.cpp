e915f3c85595338abaa2571d4aa91e817286f5e3
akhier <akhierthedragonhearted@yahoo.com>
2014-05-10 20:46:48 -0400

    +_loadtexture +_rendertextastexture

--- drw_sdl2.cpp
+++ drw_sdl2.cpp
@@ -18,7 +18,28 @@ void DrW_SDL2::_logerror(const std::string &message){
 }
 
 SDL_Texture* DrW_SDL2::_loadtexture(const std::string &file){
-    SDL_Texture* outputTexture = IMG
+    SDL_Texture* outputTexture = IMG_LoadTexture(_renderer, file.c_str());
+    if (outputTexture == nullptr){
+        _logerror("IMG_LoadTexture");
+    }
+    return outputTexture;
+}
+
+SDL_Texture* DrW_SDL2::_rendertextastexture(const std::string &message){
+    if (_font == nullptr){
+        return nullptr;
+    }
+    SDL_Surface* tempSurface = TTF_RenderText_Blended(_font, message.c_str(), color);
+    if (tempSurface == nullptr){
+        _logerror("TTF_RenderText_Blended")
+        return nullptr;
+    }
+    SDL_Texture* outputTexture = SDL_CreateTextureFromSurface(_renderer, tempSurface);
+    if (outputTexture == nullptr){
+        _logerror("SDL_CreateTextureFromSurface");
+    }
+    SDL_FreeSurface(tempSurface);
+    return outputTexture;
 }
 
 DrW_SDL2::~DrW_SDL2()
