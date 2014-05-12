7e4f24a8f47262d2d9b56da56690dc4167e36c96
akhier <akhierthedragonhearted@yahoo.com>
2014-05-12 06:18:50 -0400

    make a square appear

--- libtest.cpp
+++ libtest.cpp
@@ -6,8 +6,14 @@ int main(int argc, char **argv){
     DrW_SDL2 sdl;
     sdl.createWindow("test window", SCREEN_WIDTH, SCREEN_HEIGHT, true);
     int test = sdl.createTexture("TestTexture.png");
+    int solidPink = sdl.setupTileset(test, Rect(1, 1, 20, 20));
+    int halfandhalf = sdl.setupTileset(test, Rect(22, 1, 20, 20));
+    int solidBlue = sdl.setupTileset(test, Rect(43, 1, 20, 20));
     sdl.renderclear();
-    sdl.renderTexture(test, 10, 10);
+    sdl.renderTexture(test, sdl.getSourceRect(test, solidPink), Rect(20, 20, 20, 20));
+    sdl.renderTexture(test, sdl.getSourceRect(test, solidBlue), Rect(40, 40, 20, 20));
+    sdl.renderTexture(test, sdl.getSourceRect(test, halfandhalf), Rect(40, 20, 20, 20));
+    sdl.renderTexture(test, sdl.getSourceRect(test, halfandhalf), Rect(20, 40, 20, 20));
     sdl.renderpresent();
     SDL_Delay(2000);
     return 0;
